cea59dd4cbedc89a3e8a12efe5b79b2038007b0f
maksim.romanov <maksim.romanov@dif.tech>
2026-05-02 22:21:30 +0400

    play/pause animation added

--- composeApp/src/commonMain/kotlin/com/matttax/reado/common/ui/images/IcPause.kt
+++ composeApp/src/commonMain/kotlin/com/matttax/reado/common/ui/images/IcPause.kt
@@ -2,8 +2,6 @@ package com.matttax.reado.common.ui.images
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
@@ -11,20 +9,23 @@ import androidx.compose.ui.unit.dp
 val IcPause: ImageVector
   get() = ImageVector.Builder(
     name = "ic_pause",
-    defaultWidth = 24.dp,
-    defaultHeight = 24.dp,
-    viewportWidth = 24f,
-    viewportHeight = 24f,
+    defaultWidth = 11.dp,
+    defaultHeight = 14.dp,
+    viewportWidth = 11f,
+    viewportHeight = 14f,
   ).apply {
-    path(
-      stroke = SolidColor(Color.White),
-      strokeLineWidth = 2f,
-      strokeLineCap = StrokeCap.Round,
-      strokeLineJoin = StrokeJoin.Round,
-    ) {
-      moveTo(8f, 5f)
-      verticalLineTo(19f)
-      moveTo(16f, 5f)
-      verticalLineTo(19f)
+    path(fill = SolidColor(Color.White)) {
+      moveTo(0f, 0f)
+      horizontalLineTo(3f)
+      verticalLineTo(14f)
+      horizontalLineTo(0f)
+      close()
+    }
+    path(fill = SolidColor(Color.White)) {
+      moveTo(8f, 0f)
+      horizontalLineTo(11f)
+      verticalLineTo(14f)
+      horizontalLineTo(8f)
+      close()
     }
   }.build()
